1562a751a5ebf7b9d367fc2c819fda1ccb7bfc34
gzsr <95002561+herniqeu@users.noreply.github.com>
2022-09-08 19:35:43 -0300

    Add files via upload

---
+++ 100%atualizado/Scripts/Dialogue.gd
@@ -0,0 +1,47 @@
+extends Control
+
+# Adicione mais dialogos na caixa copiando o exemplo abaixo.
+var dialogo = [
+	'The people from Dell Island are mysteriously getting infected by a powerful neuro-virus that is controlling their minds. ',
+	'Our hero, Dellman, needs to find a cure for the virus and restore the peace in Dell Island.', "As the problem gets bigger, our hero needs to go into an adventure to pass the three gyms and acquire the knowledge that he needs to produce the cure.",
+	"Which, in the process, he will learn more about the concept of product model so that he can confront each Mentor and surpass them.",
+	"At last, his knowledge, abilities, and attitude will be put into test in a practical challenge, where the cure will be developed so that Dell Island can be saved."
+	]
+
+var dialog_indi = 0 
+var finished = false 
+
+#Carrega a variavel dialogo.Caso tenha mais de um dialogo, mude o nome.
+func _ready():
+	load_dialogo()
+
+#detecta se o player apertou espaço
+func _process(_delta):
+	$"next-indicator".visible = finished
+	if Input.is_action_just_pressed("ui_accept"):
+		load_dialogo()
+
+#Faz com que a caixa carregue a proxima fala
+func load_dialogo():
+	if dialog_indi < dialogo.size():
+		finished = false
+		$RichTextLabel.bbcode_text = dialogo[dialog_indi]
+		$RichTextLabel.percent_visible = 1
+		$Tween.interpolate_property(
+			$RichTextLabel, "percent_visible", 0, 1, 4, 
+			#Primeiro valor= porcentagem visivel inicial, Segundo valor= Porcentagem visivel final.
+			#Terceiro valor= quanto tempo leva.
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT 
+		)
+		$Tween.start()
+	else: 
+		queue_free()
+	dialog_indi +=1
+	
+
+	if dialog_indi > 3:
+		get_tree().change_scene("res://Cenas/Mapa.tscn")
+
+
+func _on_Tween_tween_completed(object, key):
+	finished = true
